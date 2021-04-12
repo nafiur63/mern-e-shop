96c620100b0dff4f09dfe084b6ef19ef94df241e
nafiur63 <nafiur63@gmail.com>
2021-04-13 05:53:00 +0600

    image and product added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
+import Homescreen from "./screens/Homescreen";
 
 function App() {
   return (
     <>
       <Header />
-      <main className="py-1">
+      <main className="py-3">
         <Container>
-          <h1>Welcome To MERN E-Shop</h1>
+          <Homescreen/>
         </Container>
       </main>
       <Footer />
